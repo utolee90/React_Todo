cce5ec2efe2096ef4d27b6c81e0170693c09f712
utolee90 <itdatabasewiki@gmail.com>
2020-09-21 16:49:52 +0900

    불필요한 변수 쳐냈음

--- src/Timer.js
+++ src/Timer.js
@@ -4,7 +4,6 @@ export default function Timer(){
     const [cnt,setCnt]=React.useState(0);
     const [curtime, setCurtime] = React.useState(new Date()); //Current time setting
     const [timer, setTimer] = React.useState([]);
-    const [timediff, setTimediff] = React.useState([]);
     
     const click=()=>{
         setCnt(cnt+1);
@@ -12,13 +11,9 @@ export default function Timer(){
     
     React.useEffect(()=>{
     setTimer([...timer, new Date()]); //timer 리스트에 기록
-    setTimediff([...timediff, '0초 경과']); //timediff에 리스트 추가.
     } , [cnt]) // 타임 기록
     React.useEffect(()=>{
         const rectime = setInterval(()=>setCurtime(new Date()),1000); //
-        /* setTimediff(timediff.map((v, i)=>{
-            toString(parseInt(curtime.getTime()-timer[i].getTime()/1000))+'초 경과';
-        })) */
         return () => clearInterval(rectime);
     }, [])
 
